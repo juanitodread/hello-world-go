4e057fc15802a3310650e675b658882a00fad90c
Juan Sandoval <juanitodread@gmail.com>
2016-11-01 20:30:15 -0600

    Printing the name of the process that invoke the program

--- src/org/juanitodread/helloworld-args/hello_world_args.go
+++ src/org/juanitodread/helloworld-args/hello_world_args.go
@@ -8,9 +8,13 @@ import (
 func main() {
   var arguments string
 
+  // Start to print since position 1, becase position 0 (zero) is reserved for
+  // the name of the command that invoke the program
   for i := 1; i < len(os.Args); i++ {
     arguments += os.Args[i] + " "
+    fmt.Println(i)
   }
 
+  fmt.Printf("The name of the command that invoked this program is: %s\n", os.Args[0])
   fmt.Printf("Hello World: %s\n", arguments)
 }
