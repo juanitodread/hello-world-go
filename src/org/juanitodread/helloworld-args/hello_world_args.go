2f66eb277d3c3591d048136834397be327e26b46
Juan Sandoval <juanitodread@gmail.com>
2016-11-01 20:33:43 -0600

    Printing index and value of os.Args object

--- src/org/juanitodread/helloworld-args/hello_world_args.go
+++ src/org/juanitodread/helloworld-args/hello_world_args.go
@@ -6,15 +6,12 @@ import (
 )
 
 func main() {
-  var arguments string
+  
+  fmt.Printf("The name of the command that invoked this program is: %s\n", os.Args[0])
 
   // Start to print since position 1, becase position 0 (zero) is reserved for
   // the name of the command that invoke the program
   for i := 1; i < len(os.Args); i++ {
-    arguments += os.Args[i] + " "
-    fmt.Println(i)
+    fmt.Printf("os.Args[%d]=%s\n", i, os.Args[i])
   }
-
-  fmt.Printf("The name of the command that invoked this program is: %s\n", os.Args[0])
-  fmt.Printf("Hello World: %s\n", arguments)
 }
