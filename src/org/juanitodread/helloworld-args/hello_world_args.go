d940c70931808ba2a31caf997a80bc394d1f51c5
Juan Sandoval <juanitodread@gmail.com>
2016-11-01 20:41:43 -0600

    A more idiomatic go-foreach

--- src/org/juanitodread/helloworld-args/hello_world_args.go
+++ src/org/juanitodread/helloworld-args/hello_world_args.go
@@ -6,12 +6,16 @@ import (
 )
 
 func main() {
-  
+
   fmt.Printf("The name of the command that invoked this program is: %s\n", os.Args[0])
 
   // Start to print since position 1, becase position 0 (zero) is reserved for
   // the name of the command that invoke the program
-  for i := 1; i < len(os.Args); i++ {
-    fmt.Printf("os.Args[%d]=%s\n", i, os.Args[i])
+
+  // A more idiomatic for: first define two variables i and elem, i is the index
+  // of the loop and elem is the element at position i of the array
+  // the array starts from 1..N
+  for i, elem := range os.Args[1:]{
+    fmt.Printf("os.Args[%d]=%s\n", i, elem)
   }
 }
