334dae28c55ad49fc8a205e4b4e47baca2af79cf
Juan Sandoval <juanitodread@gmail.com>
2016-11-14 22:38:45 -0600

    Count dup lines from stdin

---
+++ src/org/juanitodread/count-dup-lines/count-dup-lines.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "os"
+)
+
+func main() {
+  // create a map(string, int) map and store each new line
+  counts := make(map[string]int)
+
+  // create a new scanner object to read from stdin
+  inputs := bufio.NewScanner(os.Stdin)
+
+  // read the line as set as the key of the map, every time the key is
+  // repeated increment the counter
+  for inputs.Scan() {
+    counts[inputs.Text()]++
+  }
+
+  // print results
+  for line, count := range counts {
+    if count > 1 {
+      fmt.Printf("The line \"%s\" was introduced %d times\n", line, count)
+    }
+  }
+}
